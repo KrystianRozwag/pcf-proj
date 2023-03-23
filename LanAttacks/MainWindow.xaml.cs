d80db9e9dc24e50bbc25f01894d3647407db18a2
MRadzynski <radzynski.milosz@gmail.com>
2023-03-23 13:26:09 +0100

    Add initial WPF UI with routing

---
+++ LanAttacks/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LanAttacks.ViewModels;
+
+namespace LanAttacks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new HomeViewModel();
+        }
+
+        private void HomeView_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new HomeViewModel();
+        }
+
+        private void SpoofingView_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new SpoofingViewModel();
+        }
+
+        private void SniffingView_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new SniffingViewModel();
+        }
+
+        private void ICMPFloodingView_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new ICMPFloodingViewModel();
+        }
+    }
+}
